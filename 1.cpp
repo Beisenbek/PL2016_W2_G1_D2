b3e420d0e7b1253c2cc80dcc15217ad6b3548d5c
Beisenbek Baisakov <bsnbk@Beisenbeks-MacBook-Pro.local>
2016-08-31 16:20:05 +0600

    init

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+	double x,y,z;
+	cin >> x >> y >> z;
+	
+	double a1 = sqrt(abs(x-1)) - pow(abs(y),1.0/3);
+	double a2 = 1 + x*x/2 + y*y/4;
+	double a = a1/a2;
+
+	double b1 = atan(z) + exp(-x-3);
+	double b = x * b1;
+
+	cout << a << " " << b;
+
+	return 0;
+}
+
+
